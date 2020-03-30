604da38bd5a031cabf82109cd8982fe4ba9cf440
Kevin Vanantwerpen <kevin.v1989@gmail.com>
2020-03-30 19:43:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <div>
+        <h2> Single Item</h2>
+        <Slider {...settings}>
+          <div>
+            <img src="https://images.unsplash.com/photo-1548163239-47b907dee9f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1652&q=80" alt=""/>
+          </div>
+          <div>
+            <img src="https://images.unsplash.com/photo-1522136846210-1f22a26c02c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80" alt=""/>
+          </div>
+          <div>
+            <img src="https://images.unsplash.com/photo-1561962535-8671711230cd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80" alt=""/>
+          </div>
+          <div>
+            <img src="https://images.unsplash.com/photo-1516192936532-a572a51fc1b5?ixlib=rb-1.2.1&auto=format&fit=crop&w=1651&q=80" alt=""/>
+          </div>
+          <div>
+            <img src="https://images.unsplash.com/photo-1456534231849-7d5fcd82d77b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2088&q=80" alt=""/>
+          </div>
+        </Slider>
+      </div>
+    );
+  }
 }
-
-export default App;
